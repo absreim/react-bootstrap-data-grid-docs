b11f77d8d0879a9cbf11f7d9f4ef541408bbe97e
Brook Li <emcdynamicist@gmail.com>
2026-04-01 06:24:37 +0900

    Added link to index for blog section

--- src/app/blog/[slug]/layout.tsx
+++ src/app/blog/[slug]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { FC, ReactNode } from "react";
 import DocContentsLayout from "@/shared/DocContentsLayout";
 import { getBlogLinkDefs } from "@/lib/getLinkDefs";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "react-bootstrap-data-grid Blog",
@@ -9,10 +10,36 @@ export const metadata: Metadata = {
     "Blog for the react-bootstrap-data-grid project",
 };
 
+const indexLink: ReactNode = (
+  <>
+    <ul className="nav flex-column">
+      <li className="nav-item">
+        <Link href="/blog" className="nav-link px-0">
+          <svg
+            className="me-2"
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708z"
+            />
+          </svg>
+          Back to Index
+        </Link>
+      </li>
+    </ul>
+    <hr />
+  </>
+);
+
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   const linkDefs = getBlogLinkDefs();
 
-  return <DocContentsLayout linkDefs={linkDefs}>{children}</DocContentsLayout>;
+  return <DocContentsLayout linkDefs={linkDefs} prependNavContent={indexLink}>{children}</DocContentsLayout>;
 };
 
 export default Layout;
--- src/shared/DocContentsLayout.tsx
+++ src/shared/DocContentsLayout.tsx
@@ -4,18 +4,24 @@ import { LinkDefinition } from "@/shared/types";
 
 interface DocContentsLayoutProps {
   children: ReactNode;
-  linkDefs: LinkDefinition[]
+  linkDefs: LinkDefinition[];
+  prependNavContent?: ReactNode;
 }
 
-const DocContentsLayout: FC<DocContentsLayoutProps> = ({ children, linkDefs }) => {
+const DocContentsLayout: FC<DocContentsLayoutProps> = ({
+  children,
+  linkDefs,
+  prependNavContent,
+}) => {
   return (
     <div className="d-flex flex-row gap-2 h-100">
       <div className="d-none d-lg-block overflow-y-auto pe-3">
+        {prependNavContent}
         <ContentsNavbar linkDefs={linkDefs} />
       </div>
       <main className="h-100 w-100">{children}</main>
     </div>
   );
-}
+};
 
 export default DocContentsLayout;
