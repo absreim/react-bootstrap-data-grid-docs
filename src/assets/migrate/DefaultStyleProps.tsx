f400bb84bfe59a3e664154134721074596a48100
Brook Li <emcdynamicist@gmail.com>
2026-06-07 19:25:36 +0900

    Finished first pass of migration document

---
+++ src/assets/migrate/DefaultStyleProps.tsx
@@ -0,0 +1,172 @@
+import { FC } from "react";
+
+const DefaultStyleProps: FC = () => (
+  <table className="table">
+    <thead>
+      <tr>
+        <th>Property</th>
+        <th>Type</th>
+        <th>Default Classes</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <code>mainTableStyleModel.table</code>
+        </td>
+        <td>
+          <code>{"string[]"}</code>
+        </td>
+        <td>
+          <code>{'["table"]'}</code>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <code>mainTableStyleModel.tbodyTd</code>
+        </td>
+        <td>
+          <code>
+            {
+              "(rowId: RowId, displayRowIndex: number, colIndex: number) => string[] | null"
+            }
+          </code>
+        </td>
+        <td>
+          <code>{'["form-control"]'}</code>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <code>mainTableStyleModel.editCancelButton</code>
+        </td>
+        <td>
+          <code>
+            {"(rowId: RowId, displayIndex: number) => string[] | null"}
+          </code>
+        </td>
+        <td>
+          <code>{'["btn", "btn-secondary"]'}</code>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <code>mainTableStyleModel.editSaveButton</code>
+        </td>
+        <td>
+          <code>
+            {"(rowId: RowId, displayIndex: number) => string[] | null"}
+          </code>
+        </td>
+        <td>
+          <code>{'["btn", "btn-primary"]'}</code>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <code>mainTableStyleModel.editDeleteButton</code>
+        </td>
+        <td>
+          <code>
+            {"(rowId: RowId, displayIndex: number) => string[] | null"}
+          </code>
+        </td>
+        <td>
+          <code>{'["btn", "btn-secondary"]'}</code>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <code>mainTableStyleModel.editStartButton</code>
+        </td>
+        <td>
+          <code>
+            {"(rowId: RowId, displayIndex: number) => string[] | null"}
+          </code>
+        </td>
+        <td>
+          <code>{'["btn", "btn-primary"]'}</code>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <code>mainTableStyleModel.rowSelectColTh</code>
+        </td>
+        <td>
+          <code>{"string[]"}</code>
+        </td>
+        <td>
+          <code>{'["btn-primary"]'}</code>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <code>filterInputTableStyleModel.startDateInput</code>
+        </td>
+        <td>
+          <code>{"(rowIndex: number) => string[] | null"}</code>
+        </td>
+        <td>
+          <code>{'["form-control"]'}</code>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <code>filterInputTableStyleModel.endDateInput</code>
+        </td>
+        <td>
+          <code>{"(rowIndex: number) => string[] | null"}</code>
+        </td>
+        <td>
+          <code>{'["form-control"]'}</code>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <code>filterInputTableStyleModel.submitButton</code>
+        </td>
+        <td>
+          <code>{"string[]"}</code>
+        </td>
+        <td>
+          <code>{'["btn", "btn-primary"]'}</code>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <code>filterInputTableStyleModel.schemeSelectionInput</code>
+        </td>
+        <td>
+          <code>{"(rowIndex: number) => string[] | null"}</code>
+        </td>
+        <td>
+          <code>{'["form-select"]'}</code>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <code>filterInputTableStyleModel.numberInput</code>
+        </td>
+        <td>
+          <code>{"(rowIndex: number) => string[] | null"}</code>
+        </td>
+        <td>
+          <code>{'["form-control"]'}</code>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <code>filterInputTableStyleModel.searchStringInput</code>
+        </td>
+        <td>
+          <code>{"(rowIndex: number) => string[] | null"}</code>
+        </td>
+        <td>
+          <code>{'["form-control"]'}</code>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+);
+
+export default DefaultStyleProps;
